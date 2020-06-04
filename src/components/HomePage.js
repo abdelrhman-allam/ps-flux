4dcdefaf165fba6eae0cd3df14557875cb11d541
Abdelrhman Mohamed Allam <abdelrhman.allam@gmail.com>
2020-06-04 23:11:06 +0200

    Courses Component

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Home Page</h1>
+      <p>React, Flux, React Router</p>
+    </div>
+  );
+}
+// require less work to import/importer file
+export default HomePage;
